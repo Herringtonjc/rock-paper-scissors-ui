aac349a71396d301a043ca8a24166867255eb28f
Jarred Herrington <herringtonjc@yahoo.com>
2021-01-26 16:57:57 -0600

    Changed the image updating function to apply images from both the player and the computer

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,9 @@ const flxScores = document.getElementById("flex-scores");
 const flxSelect = document.getElementById("flex-selections");
 const gStatus = document.getElementById("status");
 
+const computerImage = document.getElementById("computer-choice-image");
+const playerImage = document.getElementById("player-choice-image");
+
 btnStart.addEventListener("click", () => game());
 btnRock.addEventListener("click", () => { playRound("rock") });
 btnPaper.addEventListener("click", () => { playRound("paper"); });
@@ -68,27 +71,33 @@ function computerPlay() {
   return choices[computerChoice];
 }
 
-function updateComputerImage(computerSelection) {
+function updateImages(computerSelection, humanSelection) {
   //Display the computers choice using an image
-  let imageDiv = getElementByClassName("computer-choice-image");
   switch(computerSelection) {
     case "rock":
-      let rockImage = new Image();
-      rockImage.src = "images/rock.png";
-      imageDiv.appendChild(rockImage);
+      computerImage.src = "images/rock.png";
       break;
     case "scissors":
-      let scissorsImage = new Image();
-      scissorsImage.src = "images/scissors.png";
-      imageDiv.appendChild(scissorsImage);
+      computerImage.src = "images/scissors.png";
       break;
     case "paper":
-      let paperImage = new Image();
-      paperImage.src = "images/paper.png";
-      imageDiv.appendChild(paperImage);
+      computerImage.src = "images/paper.png";
       break;
     default:
-      return;
+      break;
+  }
+  switch(humanSelection) {
+    case "rock":
+      playerImage.src = "images/rock.png";
+      break;
+    case "scissors":
+      playerImage.src = "images/scissors.png";
+      break;
+    case "paper":
+      playerImage.src = "images/paper.png";
+      break;
+    default:
+      break;
   }
 }
 
@@ -96,33 +105,40 @@ function playRound(humanSelection) {
   //Compare the humanSelection to the computerSelection
   if (gamePlaying()) {
     let computerSelection = computerPlay();
-    
+
     if (humanSelection == "rock" && computerSelection == "scissors") {
       pScore++;
       playerScore.textContent = pScore;
       gStatus.textContent = "You win this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == "rock" && computerSelection == "paper") {
       cScore++;
       computerScore.textContent = cScore;
       gStatus.textContent = "You lost this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == "scissors" && computerSelection == "paper") {
       pScore++;
       playerScore.textContent = pScore;
       gStatus.textContent = "You win this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == "scissors" && computerSelection == "rock") {
       cScore++;
       computerScore.textContent = cScore;
       gStatus.textContent = "You lost this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == "paper" && computerSelection == "rock") {
       pScore++;
       playerScore.textContent = pScore;
       gStatus.textContent = "You win this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == "paper" && computerSelection == "scissors") {
       cScore++;
       computerScore.textContent = cScore;
       gStatus.textContent = "You lost this round!";
+      updateImages(computerSelection, humanSelection)
     } else if (humanSelection == computerSelection) {
       gStatus.textContent = "You tied!";
+      updateImages(computerSelection, humanSelection)
     } else {
       //This should never be reached, only used as failsafe
       console.log("ERROR: A valid selection was not made.");
